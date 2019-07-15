15063082fb74a54eccfe8255eb8c4f744d5e7f0e
brdn1010 <52923885+brdn1010@users.noreply.github.com>
2019-07-15 23:45:35 +0800

    Update new.js

--- new.js
+++ new.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 axios.post('http://localhost:3001/api/did',{
     publicKey : 'GHAAQWPDMTNTIHMLXEQJWQWQETVTFQTGEXXYJKUDOJBKCYKTLUXEFVMOF9LKWHYHDFNETPKVLVJEIENGZBYEXVSCHB'
+    //這裡應該放置本地RSA的公鑰
 })
   .then(function (response) {
     console.log(response);
